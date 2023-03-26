93a757fd10a97d8e6183a0b071bbe1f9db8d8481
JSP080 <sebastian.puerta2@udea.edu.co>
2023-03-26 16:38:07 -0500

    Ejercicio 1 con char y string completado

--- Exercise1Char/ex1char/main.cpp
+++ Exercise1Char/ex1char/main.cpp
@@ -1,82 +1,213 @@
 #include <iostream>
-#include "functions_ex1char.h"
 #include <fstream>
 
 using namespace std;
 
+void concatenate(char *destination, const char *source);
+void characterToBinary(char c, char *binary);
+int getLength(char *array);
+void encodedMethod1(char *binaryContent, char *encodedContent, int n);
+void encodedMethod2(char *binaryContent, char *encodedContent, int n);
+void binaryStringtoBytes(const char *binaryString, unsigned char *bytes, int &byteCount);
+
+// xxd -b file (Para ver el contenido del archivo binario)
+
 int main()
 {
-    const int maxFileNameLen = 100;
-    char inputFileName[maxFileNameLen], outputFileName[maxFileNameLen];
-    int n, encodingMethod;
-    int ch;
+    const int size = 256;
+    char inputFileName[size];
+    char outputFileName[size];
 
-    cout << "Ingrese el nombre de entrada del archivo: ";
-    cin.getline(inputFileName, maxFileNameLen);
+    cout << "Introduce el nombre del archivo fuente (.txt): ";
+    cin.getline(inputFileName, size);
 
-    cout << "Ingrese el nombre de salida del archivo: ";
-    cin.getline(outputFileName, maxFileNameLen);
+    cout << "Introduce el nombre del archivo de salida (.binary): ";
+    cin.getline(outputFileName, size);
 
-    cout << "Ingrese el número n de codificación para la semilla: ";
+    int n;
+    cout << "Introduce la semilla de codificación (n): ";
     cin >> n;
 
-    cout << "Ingrese el método de codificación (1 o 2): ";
+    int encodingMethod;
+    cout << "Introduce el número de codificación (1 o 2): ";
     cin >> encodingMethod;
+    cin.ignore(); //Limpiar el buffer después de leer un entero
 
-    while ((ch = cin.get()) != '\n' && ch != EOF);
+    char line[size];
+    char content[size * 2] = {0};
 
     try {
-        ifstream inputFile(inputFileName, ios::binary | ios::ate);
-        inputFile.exceptions(ifstream::failbit | ifstream::badbit);
+        ifstream input_file(inputFileName);
 
-        streamsize fileSize = inputFile.tellg();
-        inputFile.seekg(0, ios::beg);
-
-        char *content = new char[fileSize * 8 + 1];
-        char buffer;
-        int bitPos = 0;
-
-        while (inputFile.read(&buffer, 1)) {
-            for (int i = 7; i >= 0; i--) {
-                content[bitPos++] = (buffer & (1 << i)) ? '1' : '0';
-            }
-        }
-        content[bitPos] = '\0';
-
-        inputFile.close();
-
-        if (encodingMethod == 1) {
-            encodeMethod1(content, n);
-        } else if (encodingMethod == 2) {
-            encodeMethod2(content, n);
-        } else {
-            cerr << "Invalid encoding method. Exiting." << endl;
-            delete[] content;
-            return 1;
+        if (!input_file.is_open()) {
+            throw runtime_error("Error al abrir el archivo fuente.");
         }
 
-        ofstream outputFile(outputFileName, ios::binary);
-        outputFile.exceptions(ofstream::failbit | ofstream::badbit);
-
-        for (int i = 0; i < fileSize * 8; i += 8) {
-            unsigned char byte = 0;
-            for (int j = 0; j < 8; j++) {
-                if (content[i + j] == '1') {
-                    byte |= (1 << (7 - j));
-                }
-            }
-            outputFile.write(reinterpret_cast<char *>(&byte), 1);
+        while (input_file.getline(line, size)) {
+            concatenate(content, line);
+            concatenate(content, "\n");
         }
 
-        outputFile.close();
-        delete[] content;
-
-        cout << "Encoding completed successfully." << endl;
-
-    } catch (const ifstream::failure &e) {
-        cerr << "An error ocurred while processing the file: " << e.what() << endl;
+        input_file.close();
+    } catch (const exception &e) {
+        cerr << "Error: " << e.what() << endl;
         return 1;
     }
 
+    char binaryContent[size * 16] = {0};
+    // Arreglo para almacenar el contenido codificado
+    char encodedContent[size * 16] = {0};
+    char binary[9];
+
+    for (int i = 0; content[i] != '\0'; ++i) {
+        if (content[i] != '\n') {
+            characterToBinary(content[i], binary);
+            concatenate(binaryContent, binary);
+        } else {
+            concatenate(binaryContent, " ");
+        }
+    }
+
+
+
+    if (encodingMethod == 1) {
+        encodedMethod1(binaryContent, encodedContent, n);
+    } else if (encodingMethod == 2) {
+        encodedMethod2(binaryContent, encodedContent, n);
+    } else {
+        cerr << "Error: Método de codificación inválido." << endl;
+        return 1;
+    }
+
+    int byteCount = 0;
+    unsigned char *bytes = new unsigned char[getLength(encodedContent) / 8 + 1];
+    binaryStringtoBytes(encodedContent, bytes, byteCount);
+
+    try {
+        ofstream output_file(outputFileName, ios::binary);
+
+        if (!output_file.is_open()) {
+            throw runtime_error("Error al abrir el archivo de salida.");
+        }
+
+        for (int i = 0; encodedContent[i] != '\0'; ++i) {
+            output_file.write(reinterpret_cast<const char*>(&bytes[i]), 1);
+        }
+
+        output_file.close();
+    } catch (const exception &e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
+
+    delete[] bytes;
+
+    cout << "Archivo convertido a binario exitosamente." << endl;
+
     return 0;
+}
+
+void characterToBinary(char c, char *binary) {
+    for (int i = 7; i >= 0; --i) {
+        binary[7 - i] = (c & (1 << i)) ? '1' : '0';
+    }
+    binary[8] = '\0';
+}
+
+void concatenate(char *destination, const char *source) {
+    while (*destination) {
+        destination++;
+    }
+
+    while ((*destination++ = *source++)) {
+    }
+}
+
+int getLength(char *array) {
+    int length = 0;
+    while (array[length] != '\0') {
+        ++length;
+    }
+    return length;
+}
+
+void encodedMethod1(char *binaryContent, char *encodedContent, int n) {
+    int length = getLength(binaryContent);
+
+    // Copiar el contenido binario original a encodedContent
+    for (int i = 0; i < length; ++i) {
+        encodedContent[i] = binaryContent[i];
+    }
+    encodedContent[length] = '\0';
+
+    // Invertir bits del primer bloque
+    for (int i = 0; i < n && i < length; ++i) {
+        encodedContent[i] = (encodedContent[i] == '1') ? '0' : '1';
+    }
+
+    int ones, zeros, interval;
+    for (int i = n; i < length; i += n) {
+        ones = 0;
+        zeros = 0;
+
+        // Contar 1s y 0s en el bloque anterior
+        for (int j = i - n; j < i; ++j) {
+            if (binaryContent[j] == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+        }
+
+        // Determinar intervalo de inversión
+        if (ones == zeros) {
+            interval = 1;
+        } else if (zeros > ones) {
+            interval = 2;
+        } else {
+            interval = 3;
+        }
+
+        // Invertir bits según el intervalo determinado usando encodedContent (codificado)
+        for (int j = i; j < i + n && j < length; ++j) {
+            if ((j - i + 1) % interval == 0) {
+                encodedContent[j] = (encodedContent[j] == '1') ? '0' : '1';
+            }
+        }
+    }
+}
+
+void encodedMethod2(char *binaryContent, char *encodedContent, int n) {
+    int length = getLength(binaryContent);
+    int i, j;
+
+    for (i = 0; i < length; i += n) {
+        for (j = 0; j < n && (i + j) < length; ++j) {
+            if (j == 0) {
+                // El primer bit codificado corresponde al último sin codificar
+                encodedContent[i] = binaryContent[i + n - 1];
+            } else {
+                // El siguiente bit codificado corresponde al bit anterior sin codificar
+                encodedContent[i + j] = binaryContent[i + j - 1];
+            }
+        }
+    }
+
+    encodedContent[length] = '\0';
+}
+
+void binaryStringtoBytes(const char *binaryString, unsigned char *bytes, int &byteCount) {
+    int index = 0;
+    byteCount = 0;
+    while (binaryString[index] != '\0') {
+        unsigned char byte = 0;
+        for (int bit = 0; bit < 8 && binaryString[index] != '\0'; ++bit) {
+            byte <<= 1;
+            if (binaryString[index] == '1') {
+                byte |= 1;
+            }
+            ++index;
+        }
+        bytes[byteCount++] = byte;
+    }
 }
--- Exercise1String/ex1string/main.cpp
+++ Exercise1String/ex1string/main.cpp
@@ -4,8 +4,166 @@
 
 using namespace std;
 
+void characterToBinary(char c, string &binary);
+string encodedMethod1(const string &binaryContent, int n);
+string encodedMethod2(const string &binaryContent, int n);
+
+// xxd -b file (Para ver el contenido del archivo binario)
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    string inputFileName;
+    string outputFileName;
+
+    cout << "Introduce el nombre del archivo fuente (.txt): ";
+    getline(cin, inputFileName);
+
+    cout << "Introduce el nombre del archivo de salida (.binary): ";
+    getline(cin, outputFileName);
+
+    int n;
+    cout << "Introduce la semilla de codificación (n): ";
+    cin >> n;
+
+    int encodingMethod;
+    cout << "Introduce el número de codificación (1 o 2): ";
+    cin >> encodingMethod;
+    cin.ignore(); // Limpiar el buffer después de leer un entero
+
+    string line;
+    string content;
+
+    try {
+        ifstream input_file(inputFileName);
+
+        if (!input_file.is_open()) {
+            throw runtime_error("Error al abrir el archivo fuente.");
+        }
+
+        while (getline(input_file, line)) {
+            content += line + '\n';
+        }
+
+        input_file.close();
+    } catch (const exception &e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
+
+    string binaryContent;
+    string binary;
+    for (char c : content) {
+        if (c != '\n') {
+            characterToBinary(c, binary);
+            binaryContent += binary;
+        } else {
+            binaryContent += ' ';
+        }
+    }
+
+    string encodedContent;
+
+    if (encodingMethod == 1) {
+        encodedContent = encodedMethod1(binaryContent, n);
+    } else if (encodingMethod == 2) {
+        encodedContent = encodedMethod2(binaryContent, n);
+    } else {
+        cerr << "Error: Método de codificación inválido." << endl;
+        return 1;
+    }
+
+    try {
+        ofstream output_file(outputFileName, ios::binary);
+
+        if (!output_file.is_open()) {
+            throw runtime_error("Error al abrir el archivo de salida.");
+        }
+
+        // Convertir el contenido codificado a valores binarios y escribirlos en el archivo
+        for (int i = 0; i < encodedContent.size(); i += 8) {
+            char byte = 0;
+            for (int j = 0; j < 8 && i + j < encodedContent.size(); ++j) {
+                byte |= (encodedContent[i + j] == '1') ? (1 << (7 - j)) : 0;
+            }
+            output_file.write(&byte, 1);
+        }
+
+        output_file.close();
+    } catch (const exception &e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
+
+    cout << "Archivo convertido a binario exitosamente." << endl;
+
     return 0;
+}
+
+void characterToBinary(char c, string &binary) {
+    binary.clear();
+    for (int i = 7; i >= 0; --i) {
+        binary += (c & (1 << i)) ? '1' : '0';
+    }
+}
+
+string encodedMethod1(const string &binaryContent, int n) {
+    string encodedContent(binaryContent);
+
+    // Invertir bits del primer bloque
+    for (int i = 0; i < n && i < binaryContent.size(); ++i) {
+        encodedContent[i] = (encodedContent[i] == '1') ? '0' : '1';
+    }
+
+    int ones, zeros, interval;
+    for (int i = n; i < binaryContent.size(); i += n) {
+        ones = 0;
+        zeros = 0;
+
+        // Contar 1s y 0s en el bloque anterior
+        for (int j = i - n; j < i; ++j) {
+            if (binaryContent[j] == '1') {
+                ones++;
+            } else {
+                zeros++;
+            }
+        }
+
+        // Determine intervalo de inversión
+        if (ones == zeros) {
+            interval = 1;
+        } else if (zeros > ones) {
+            interval = 2;
+        } else {
+            interval = 3;
+        }
+
+        // Invertir bits según el intervalo determinado usando encodedContent (codificado)
+        for (int j = i; j < i + n && j < binaryContent.size(); ++j) {
+            if ((j - i + 1) % interval == 0) {
+                encodedContent[j] = (encodedContent[j] == '1') ? '0' : '1';
+            }
+        }
+    }
+
+    return encodedContent;
+}
+
+string encodedMethod2(const string &binaryContent, int n) {
+    string encodedContent;
+    encodedContent.reserve(binaryContent.size());
+    int i, j;
+
+    for (i = 0; i < binaryContent.size(); i += n) {
+        for (j = 0; j < n && (i + j) < binaryContent.size(); ++j) {
+            if (j == 0) {
+                // El primer bit codificado corresponde al último sin codificar
+                encodedContent.push_back(binaryContent[i + n - 1]);
+            } else {
+                // El siguiente bit codificado corresponde al bit anterior sin codificar
+                encodedContent.push_back(binaryContent[i + j - 1]);
+            }
+        }
+    }
+
+    return encodedContent;
 }
